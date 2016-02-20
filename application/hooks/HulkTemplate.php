c48d7304045938d9638ec04f790b7b583d08b20c
netsenior <netsenior@gmail.com>
2016-02-19 22:32:12 -0300

    Update and rename newView.php to HulkTemplate.php

--- application/hooks/HulkTemplate.php
+++ application/hooks/HulkTemplate.php
@@ -2,25 +2,25 @@
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
-class NewView {
+class HulkTemplate{
 
-    function doView() {
+    function exec() {
         $CI = & get_instance();
         $output = $CI->output->get_output();
         if (isset($CI->layout) && !preg_match('/(.+).php$/', $CI->layout)) {
             $CI->layout .= '.php';
         } else {
-            $CI->layout = 'default.php';
+            $CI->layout = 'template.php';
         }
-        $layout = BASEPATH . '../application/views/template/' . $CI->layout;
-        if ($CI->layout !== "default.php" && !file_exists($layout)) {
+        $layout = BASEPATH . '../application/views/' . $CI->layout;
+        if ($CI->layout !== "template.php" && !file_exists($layout)) {
             show_error('Voce especificou um layout (' . $CI->layout . ') inexistente.');
         }
         if (file_exists($layout)) {
             $layout = $CI->load->file($layout, true);
-            # CONTEUDO
+            # CONTENT
             $view = str_replace("{content_for_layout}", $output, $layout);
-            # TITULO
+            # TITLE
             if (!empty($CI->title))
                 $view = str_replace("{title}", $CI->title, $view);
             else
